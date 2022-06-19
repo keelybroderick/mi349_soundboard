763b779cd2239acad8a49c598633cf51156be0e8
keelybroderick <98989354+keelybroderick@users.noreply.github.com>
2022-06-19 13:08:39 -0700

    Add files via upload

---
+++ soundboard.js
@@ -0,0 +1,12 @@
+var x = document.getElementById("myAudioCow"); 
+var y = document.getElementById("myAudioGhost"); 
+var z = document.getElementById("myAudioOcean"); 
+function playAudioCow() { 
+    x.play(); 
+} 
+function playAudioGhost() { 
+    y.play(); 
+} 
+function playAudioOcean() { 
+    z.play(); 
+} 
