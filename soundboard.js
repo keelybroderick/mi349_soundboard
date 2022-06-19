0f4ae30a5005cc85e9977f7510055f301d7dfde7
keelybroderick <98989354+keelybroderick@users.noreply.github.com>
2022-06-19 12:08:23 -0700

    Add files via upload

---
+++ soundboard.js
@@ -0,0 +1,35 @@
+/*
+const sounds = ['cow', 'ghost', 'ocean',
+];
+
+
+sounds.forEach(sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').
+    appendChild(btn)
+});
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById
+        (sound)
+        
+        song.pause()
+        song.currentTime = 0;
+    })
+}
+*/
+
+var buttonElement = document.getElementById('cowsound')
+
+buttonElement.addEventListener('click', function () {
+    console.log('I was clicked')
+})
